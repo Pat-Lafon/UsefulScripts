669544cef6ea3f965b2378d20dc6639d8b59ab36
Pat-Lafon <32135464+Pat-Lafon@users.noreply.github.com>
2019-09-01 18:00:19 -0400

    Update and rename rc to ssh_rc.sh

--- ssh_rc.sh
+++ ssh_rc.sh
@@ -1,4 +1,4 @@
-# File to put in .ssh/rc for when you ssh to a server
+# File to put in as .ssh/rc for when you ssh to a server
 clear
 echo "____    __    ____  _______  __        ______   ______   .___  ___.  _______  __  ";
 echo "\   \  /  \  /   / |   ____||  |      /      | /  __  \  |   \/   | |   ____||  | ";
